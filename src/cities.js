a3e3c0a8742835e00f214fbae4b6901f1714fa1e
Alejandro <aariasmartijena@gmail.com>
2021-12-13 15:44:03 -0400

    add city display functionality

--- src/cities.js
+++ src/cities.js
@@ -3,15 +3,34 @@ import cities from 'cities.json';
 function showCities(city) {
   const countryList = document.querySelector('.country-list');
   countryList.innerHTML = '';
-  const citiesToShow = cities.filter((each) => each.name.toLowerCase() === city.toLowerCase());
-  citiesToShow.forEach((element) => {
-    const cityCard = document.createElement('div');
-    cityCard.classList.add('city-card');
-    cityCard.innerHTML = `
-    <p>${element.name}, ${element.country} <span><img src="https://flagcdn.com/${element.country.toLowerCase()}.svg" width="30"></span></p>
-    `;
-    countryList.appendChild(cityCard);
-  });
+  const citiesToShow = cities
+    .filter((each) => each.name.toLowerCase() === city.toLowerCase());
+
+  if (citiesToShow.length) {
+    citiesToShow.forEach((element) => {
+      const cityCard = document.createElement('div');
+      cityCard.classList.add('city-card');
+      cityCard.innerHTML = `
+      <div class="country-details">
+        <p>${element.name}, ${element.country} 
+          <span>
+            <img class="flag" src="https://flagcdn.com/${element.country.toLowerCase()}.svg" width="30">
+          </span>
+        </p>
+      </div>
+      <div class="lat-and-long">
+        <p>Latitude: ${element.lat}</p>
+        <p>Longitude: ${element.lng}</p>
+      </div>
+      `;
+      countryList.appendChild(cityCard);
+    });
+  } else {
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('error-message');
+    errorMessage.innerHTML = "<p>We couldn't find that city</p>";
+    countryList.appendChild(errorMessage);
+  }
 }
 
 export default showCities;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const search = document.querySelector('.search');
 const searchTerm = document.querySelector('.search-term');
 search.addEventListener('click', (e) => {
   e.preventDefault();
+  if (!searchTerm.value) return;
   showCities(searchTerm.value);
   searchTerm.value = '';
 });
